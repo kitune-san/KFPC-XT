f315f8b63e697a27895a379ab78ae53f3d434406
kitune-san <19359213+kitune-san@users.noreply.github.com>
2021-05-18 00:20:35 +0900

    Add empty module.

---
+++ HDL/Bus_Arbiter.sv
@@ -0,0 +1,12 @@
+//
+// KFPC-XT Bus_Arbiter
+// Written by kitune-san
+//
+module BUS_ARBITER (
+    // Bus
+    input   logic           clock,
+    input   logic           reset,
+);
+
+endmodule
+
---
+++ HDL/Chipset.sv
@@ -0,0 +1,12 @@
+//
+// KFPC-XT Chipset
+// Written by kitune-san
+//
+module CHIPSET (
+    // Bus
+    input   logic           clock,
+    input   logic           reset,
+);
+
+endmodule
+
